8186c9261a26dd913f87baef4a634d13bcb28faf
leifoo <lei.fu.rice@gmail.com>
2019-09-13 17:05:31 -0500

    beautifulsoup example

--- stock/beautifulsoup.py
+++ stock/beautifulsoup.py
@@ -15,4 +15,52 @@ and they lived at the bottom of a well.</p>
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+# Simple way to navigate structure
+
+soup.title
+# <title>The Dormouse's story</title>
+
+soup.title.name
+# u'title'
+
+soup.title.string
+# u'The Dormouse's story'
+
+soup.title.parent.name
+# u'head'
+
+soup.p
+# <p class="title"><b>The Dormouse's story</b></p>
+
+soup.p['class']
+# u'title'
+
+soup.a
+# <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>
+
+soup.find_all('a')
+# [<a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>,
+#  <a class="sister" href="http://example.com/lacie" id="link2">Lacie</a>,
+#  <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>]
+
+soup.find(id="link3")
+# <a class="sister" href="http://example.com/tillie" id="link3">Tillie</a>
+
+
+# Extract all the URLs found within a page’s <a> tags:
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+# Extrac all the text from a page:
+print(soup.get_text())
+
+# To parse a document, pass it into the BeautifulSoup constructor. You can pass in a string or an open filehandle:
+
+# from bs4 import BeautifulSoup
+
+with open("index.html") as fp:
+    soup = BeautifulSoup(fp)
+
+soup = BeautifulSoup("<html>data</html>")
