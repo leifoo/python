cbb316a50a1c5919d70136fd08cd57b9c634fcf9
leifoo <lei.fu.rice@gmail.com>
2019-10-05 17:43:35 -0500

    add language selection option

--- youtube/gettranscript.py
+++ youtube/gettranscript.py
@@ -1,12 +1,19 @@
 import sys
 from youtube_transcript_api import YouTubeTranscriptApi
 
+# video_id = 'md6E-wgq4Js&t=180s'
+transcript = ''
+language = []
+
 try:
     video_id = sys.argv[1]
 except:
     print('Wrong usage!!!')
-    print('Correct usage: python gettranscript.py url')
+    print('Correct usage: python gettranscript.py url ch')
     sys.exit(1)
+
+if len(sys.argv) > 2:
+    language.append(sys.argv[2])
 
 if video_id[:32] == 'https://www.youtube.com/watch?v=':
     video_id = video_id[32:]
@@ -14,10 +21,8 @@ else:
     print(video_id, ' is incorrect. Exit now!')
     sys.exit(1)
 
-# video_id = 'md6E-wgq4Js&t=180s'
-transcript = ''
-
-text = YouTubeTranscriptApi.get_transcript(video_id)
+print(list(language))
+text = YouTubeTranscriptApi.get_transcript(video_id, languages=list(language))
 
 for line in text:
     transcript += line['text'] + ' '
